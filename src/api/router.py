9829f27ec47006336ee0fe42b473cd44d8a98d2d
Aleksandr Ryabinskij <olexander.rabota@yandex.ru>
2024-09-17 12:44:57 +0300

    Дописал Readme.md поменял множественное добааление изображений на одинарное и проверил работу всех ручек осталсь исправиль анотации типов по mypy

--- src/api/router.py
+++ src/api/router.py
@@ -30,51 +30,45 @@ async def create_new_task(
 
 @router.post("/load_images")
 async def load_images(
-    files: list[UploadFile],
+    file: UploadFile,
     data_image: Annotated[ImageCreate, Depends()],
     db: Session = Depends(get_db),
 ):
-    saved_files = []
-    saved_data_images = []
-    saved_data_faces = []
 
-    for file in files:
-        if not file.filename.endswith(".jpg") and not file.filename.endswith(".jpeg"):
-            raise HTTPException(
-                status_code=400,
-                detail=f"""File format not supported: {file.filename}. Please upload JPEG image only!""",
-            )
 
-        file_location = os.path.join(settings.IMAGE_PATH, file.filename)
-
-        try:
-            with open(file_location, "wb+") as file_object:
-                file_object.write(await file.read())
-            saved_files.append(file.filename)
-            task = await db.get(Tasks, data_image.task_id)
-            if not task:
-                raise HTTPException(status_code=404, detail="Task not found")
-            image = await create_images(data_image, file_location, db)
-            saved_data_images.append(image)
-        except Exception as e:
-            raise HTTPException(
-                status_code=500,
-                detail=f"""An error occured while saving the file
-                                {file.filename}: {str(e)}""",
-            )
-        token = await get_token_tevian(
-            settings.TEVIAN_SWAGGER_EMAIL, settings.TEVIAN_SWAGGER_PASSWORD
+    if not file.filename.endswith(".jpg") and not file.filename.endswith(".jpeg"):
+        raise HTTPException(
+            status_code=400,
+            detail=f"""File format not supported: {file.filename}. Please upload JPEG image only!""",
         )
-        detect_data = await detect(token, file_location)
-        for data in detect_data["data"]:
-            bounding_box = data["bbox"]
-            age = data["demographics"]["age"]["mean"]
-            gender = data["demographics"]["gender"]
-            face = await create_face(image.id, bounding_box, gender, age, db)
-            saved_data_faces.append(face)
+
+    file_location = os.path.join(settings.IMAGE_PATH, file.filename)
+
+    try:
+        with open(file_location, "wb+") as file_object:
+            file_object.write(await file.read())
+        task = await db.get(Tasks, data_image.task_id)
+        if not task:
+            raise HTTPException(status_code=404, detail="Task not found")
+        image = await create_images(data_image, file_location, db)
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"""An error occured while saving the file
+                            {file.filename}: {str(e)}""",
+        )
+    token = await get_token_tevian(
+        settings.TEVIAN_SWAGGER_EMAIL, settings.TEVIAN_SWAGGER_PASSWORD
+    )
+    detect_data = await detect(token, file_location)
+    for data in detect_data["data"]:
+        bounding_box = data["bbox"]
+        age = data["demographics"]["age"]["mean"]
+        gender = data["demographics"]["gender"]
+        face = await create_face(image.id, bounding_box, gender, age, db)
 
     return {
-        "info ": f"Files saved: {saved_files}\nSaved data images: {saved_data_images}"
+        "info ": "The image has been processed and added to the task"
     }
 
 
@@ -84,6 +78,9 @@ async def delete_task_use_id(
 ):
     try:
         await delete_task(task_id, db)
+        return {
+        "info ": "Task deleted"
+    }
     except Exception as e:
         print(e)
         raise HTTPException(status_code=500, detail="Task not deleted")
